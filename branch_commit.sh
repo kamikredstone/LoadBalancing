d8647c2de46110a5077b3349670345ad022a2414
unknown <kamikredstone@gmail.com>
2022-07-24 13:56:51 +0300

    New action

--- branch_commit.sh
+++
@@ -1,6 +0,0 @@
-sha=${{ github.sha }}
-branch=${{ github.ref }}
-name="appbalanced"
-
-build="docker image build . -t kamikredstone/ptecr:$name-$branch-$sha -t kamikredstone/ptecr:lastest"
-eval $build
